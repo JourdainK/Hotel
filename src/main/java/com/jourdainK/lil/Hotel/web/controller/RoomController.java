0b157f153191e9db02fdd3128c8303d89400a24b
jourdainkevin <jourdain.kevin33@gmail.com>
2025-02-20 01:20:23 +0100

    [ADD] RestController/API for room

--- src/main/java/com/jourdainK/lil/Hotel/web/controller/RoomController.java
+++ src/main/java/com/jourdainK/lil/Hotel/web/controller/RoomController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.jourdainK.lil.Hotel.data.repository.RoomRepository;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
